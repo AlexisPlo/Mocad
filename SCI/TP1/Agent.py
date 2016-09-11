524b2d1502c505db938ad8b889625b3f47ffd6d2
AlexisPlo <alexis.pernet@gmail.com>
2016-09-12 01:19:19 +0200

    Small SCI change

--- SCI/TP1/Agent.py
+++ SCI/TP1/Agent.py
@@ -1,13 +1,7 @@
-def Agent:
-
+class Agent:
 
 	def __init__(self, _env, _sma, _posX, _posY, _pasX, _pasY, _color):
 		self.env = env
 
-
-
 	def update(self):
-		self.env.move(self, self.pasX, self.pasY)
-
-
-	def decide(self, type):
+		self.env.move(self, self.pasX, self.pasY)def decide(self, type):
--- SCI/TP1/Environment.py
+++ SCI/TP1/Environment.py
@@ -1,9 +1,5 @@
-def Environment:
-	
-
-	def __init__(self, _gridsizeX, _gridsizeY, _tor):
-
-
-
-	def move(self, ag):
-		
+class Environment:
+    
+    def __init__(self, _gridsizeX, _gridsizeY, _tor):
+        self.agTab = [[None for i in _gridsizeY] for i in _gridsizeX]
+        self.tor = _tor
